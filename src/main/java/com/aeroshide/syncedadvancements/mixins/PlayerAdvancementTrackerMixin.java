0a75f79f7556df51f9ef91554066293a937a0687
Aeroshide <78432778+Aeroshide@users.noreply.github.com>
2023-07-28 18:03:54 +0700

    Initial commit

---
+++ src/main/java/com/aeroshide/syncedadvancements/mixins/PlayerAdvancementTrackerMixin.java
@@ -0,0 +1,41 @@
+package com.aeroshide.syncedadvancements.mixins;
+
+
+import net.minecraft.advancement.Advancement;
+import net.minecraft.advancement.AdvancementProgress;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.network.ServerPlayerInteractionManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+import net.minecraft.advancement.PlayerAdvancementTracker;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerAdvancementTracker.class)
+public abstract class PlayerAdvancementTrackerMixin {
+    @Shadow
+    private ServerPlayerEntity owner;
+    @Unique
+    private boolean isGranting = false;
+
+    @Inject(method = "grantCriterion", at = @At("HEAD"))
+    private void onAdvancement(Advancement advancement, String criterionName, CallbackInfoReturnable<Boolean> cir) {
+        if (!isGranting && advancement != null) {
+            isGranting = true;
+            PlayerManager players = owner.server.getPlayerManager();
+            for (ServerPlayerEntity player : players.getPlayerList()) {
+                if (player != owner) {
+                    PlayerAdvancementTracker tracker = player.getAdvancementTracker();
+                    tracker.grantCriterion(advancement, criterionName);
+                }
+            }
+            isGranting = false;
+        }
+    }
+}
+
+
---
+++ src/main/java/com/aeroshide/syncedadvancements/mixins/PlayerManagerMixin.java
@@ -0,0 +1,57 @@
+package com.aeroshide.syncedadvancements.mixins;
+
+import net.minecraft.advancement.Advancement;
+import net.minecraft.advancement.AdvancementManager;
+import net.minecraft.advancement.AdvancementProgress;
+import net.minecraft.advancement.PlayerAdvancementTracker;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.ServerAdvancementLoader;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerManager.class)
+public abstract class PlayerManagerMixin {
+    @Inject(method = "onPlayerConnect", at = @At("RETURN"))
+    private void onPlayerConnect(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
+        PlayerManager players = (PlayerManager) (Object) this;
+        MinecraftServer server = player.getServer();
+        if (server != null) {
+            ServerAdvancementLoader advancementManager = server.getAdvancementLoader();
+            for (ServerPlayerEntity otherPlayer : players.getPlayerList()) {
+                if (otherPlayer != player) {
+                    PlayerAdvancementTracker tracker = otherPlayer.getAdvancementTracker();
+                    for (Advancement advancement : advancementManager.getAdvancements()) {
+                        AdvancementProgress advancementProgress = tracker.getProgress(advancement);
+                        if (advancementProgress.isDone()) {
+                            for (String s : advancementProgress.getObtainedCriteria()) {
+                                player.getAdvancementTracker().grantCriterion(advancement, s);
+                            }
+                        }
+                    }
+                }
+            }
+            for (ServerPlayerEntity otherPlayer : players.getPlayerList()) {
+                if (otherPlayer != player) {
+                    PlayerAdvancementTracker tracker = player.getAdvancementTracker();
+                    for (Advancement advancement : advancementManager.getAdvancements()) {
+                        AdvancementProgress advancementProgress = tracker.getProgress(advancement);
+                        if (advancementProgress.isDone()) {
+                            for (String s : advancementProgress.getObtainedCriteria()) {
+                                otherPlayer.getAdvancementTracker().grantCriterion(advancement, s);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
